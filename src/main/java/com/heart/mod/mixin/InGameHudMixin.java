0fe8d57494c31c1edd956ae6f266a8c397fc026a
Manzultmg7 <manzultamang777@gmail.com>
2025-05-01 21:01:59 +0545

    minor bug fixes

--- src/main/java/com/heart/mod/mixin/InGameHudMixin.java
+++ src/main/java/com/heart/mod/mixin/InGameHudMixin.java
@@ -12,9 +12,12 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
 import java.util.Objects;
 
+@Environment(EnvType.CLIENT)
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 
